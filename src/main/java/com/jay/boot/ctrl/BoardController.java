a556f5a173d63303a8328e6c4c2e9e5f56243c5d
Jeongeun <jeongeunlee1209@gmail.com>
2020-03-22 00:04:07 +0900

    replace project

--- src/main/java/com/jay/boot/ctrl/BoardController.java
+++ src/main/java/com/jay/boot/ctrl/BoardController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BoardController {
 
-
+    //http://IP:port 번호만 치면 hello.jsp페이지로 이동
     @GetMapping("/")
     public String hello() {
         return "hello";
